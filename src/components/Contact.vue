59ab1ff42a2c2d567f3fe0f4e98ad895dfc40b03
Angkerathanak <oukrathanak7273@gmail.com>
2024-10-11 16:42:18 +0700

    first

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -35,7 +35,7 @@
           <div class="map_container">
             <div class="map">
               <div id="googleMap">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4370.590405639721!2d104.90911947548288!3d11.653065142411839!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3109516bdea989b3%3A0x372d2c5e0e14b706!2sCambodia%20Academy%20of%20Digital%20Technology%20(CADT)!5e1!3m2!1sen!2skh!4v1728543748122!5m2!1sen!2skh" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4370.590405639721!2d104.90911947548288!3d11.653065142411839!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3109516bdea989b3%3A0x372d2c5e0e14b706!2sCambodia%20Academy%20of%20Digital%20Technology%20(CADT)!5e1!3m2!1sen!2skh!4v1728543748122!5m2!1sen!2skh" width="600" height="450" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
               </div>
             </div>
           </div>
